8f65597b2d20c714fdedc4a15febc6ddca957f18
Sean McBride <spmcbride1201@gmail.com>
2016-03-08 10:29:32 -0500

    feat: Implement shell Player class and render single bug.

--- js/app.js
+++ js/app.js
@@ -6,6 +6,12 @@ var Enemy = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+
+    // MVP is set speed. TODO: Randomly generate enemy speed
+    this.speed = 25;
+
+    this.x = -100;
+    this.y = 60;
 };
 
 // Update the enemy's position, required method for game
@@ -14,6 +20,7 @@ Enemy.prototype.update = function(dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
+    this.x += dt * this.speed;
 };
 
 // Draw the enemy on the screen, required method for game
@@ -24,12 +31,16 @@ Enemy.prototype.render = function() {
 // Now write your own player class
 // This class requires an update(), render() and
 // a handleInput() method.
-
+var Player = function() {}
+Player.prototype.update = function(){};
+Player.prototype.render = function(){};
+Player.prototype.handleInput = function(){};
 
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
-
+var allEnemies = [new Enemy];
+var player = new Player;
 
 
 // This listens for key presses and sends the keys to your
