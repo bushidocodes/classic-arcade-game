a5e2abc291ac6563a3a218dec84fd487e5c1c953
Sean McBride <spmcbride1201@gmail.com>
2016-03-08 14:55:28 -0500

    feat: Add lives and score counter.

--- js/app.js
+++ js/app.js
@@ -46,12 +46,15 @@ Enemy.prototype.update = function(dt) {
 Enemy.prototype.render = function() {
     if (this.speed > 0) {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+        console.log(this.sprite);
+        // ctx.fillRect(this.x ,this.y,5,5);
     } else {
         ctx.save();
         ctx.translate(this.x, this.y);
         ctx.scale(-1, 1);
         //ctx.rotate(Math.PI);
         ctx.drawImage(Resources.get(this.sprite), 0, 0);
+        // ctx.fillRect(0,0,5,5);
         //ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
         //ctx.rotate(Math.PI);
         ctx.restore();
@@ -77,41 +80,40 @@ Enemy.prototype.isOutOfBounds = function() {
 var Player = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
+    this.score = 0;
+    this.lives = 3;
     this.sprite = 'images/char-boy.png';
     this.x = 200;//0,100,200,300,400
     this.y = 370;//50,130,210,290,370
 
 }
-Player.prototype.update = function(){};
-Player.prototype.render = function(){
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-Player.prototype.handleInput = function(input){
+Player.prototype.update = function(){
+    Player.prototype.handleInput = function(input){
     var verticalStep = 80;
     var horizontalStep = 100;
     switch (input) {
         case "up":
             if ( this.y - verticalStep > 0) {
-                console.log ((this.y - verticalStep) + " > " + 0);
+                // console.log ((this.y - verticalStep) + " > " + 0);
                 this.y -= verticalStep;
             }
             break;
         case "down":
             if ( this.y + verticalStep < canvas.height -160) {
-                console.log ((this.y + verticalStep) + " < " + (canvas.height- 160));
+                // console.log ((this.y + verticalStep) + " < " + (canvas.height- 160));
                 this.y += verticalStep;
             }
             break;
         case "left":
             if ( this.x - horizontalStep >= 0) {
                 this.x -= horizontalStep;
-                console.log(this.x);
+                // console.log(this.x);
             }
             break;
         case "right":
             if ( this.x + horizontalStep <= canvas.width-105) {
                 this.x += horizontalStep;
-                console.log(this.x  + " <= " + (canvas.width-105));
+                // console.log(this.x  + " <= " + (canvas.width-105));
             }
             break;
     }
@@ -120,6 +122,26 @@ Player.prototype.handleInput = function(input){
     // }
     console.log(input);
 };
+};
+Player.prototype.render = function(){
+    ctx.font = "30px Arial";
+    ctx.clearRect(0,0,150,30);
+    ctx.fillText("Score: " + player.score, 10, 30);
+    ctx.clearRect(380,0,200,30);
+    ctx.fillText("Lives: " + player.lives, 380,30);
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    // ctx.fillRect(this.x,this.y,5,5);
+    if (this.isDead()) {
+        console.log("You died");
+        player.lives--;
+
+        this.x = 200;//0,100,200,300,400
+        this.y = 370;//50,130,210,290,370
+    } else {
+        console.log("not dead");
+    }
+};
+
 
 // outOfBounds Convenience
 Player.prototype.isOutOfBounds = function() {
@@ -132,6 +154,30 @@ Player.prototype.isOutOfBounds = function() {
         return false;
     }
 };
+
+Player.prototype.isDead = function() {
+    console.log("ping");
+    var playerSquareX = Math.round(player.x/100);
+    var playerSquareY = Math.round(player.y/80);
+    var collision = false;
+    console.log("Player in tile " + playerSquareX + ", " + playerSquareY);
+    allEnemies.forEach( function(enemy) {
+        let enemySquareX = 0;
+        if (enemy.speed < 0) {
+            enemySquareX = Math.round((enemy.x-101)/100);
+        } else {
+            enemySquareX = Math.round(enemy.x/100);
+        }
+        var enemySquareY = Math.round(enemy.y/80);
+        // console.log(enemy.x);
+        console.log("Enemy in tile " + enemySquareX + ", " + enemySquareY);
+        if (playerSquareX === enemySquareX && playerSquareY === enemySquareY) {
+            console.log("collision");
+            collision = true;
+        };
+    });
+    return collision;
+}
 
 
 
