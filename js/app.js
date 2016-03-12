bdb9b96887ba1826ad5431d65cbcbd69d4633245
Sean McBride <spmcbride1201@gmail.com>
2016-03-12 14:41:02 -0500

    feat: Centralize repeat code into Enemy.prototype methods.
    Cut down on repeat code via randomizeSpeed() and randomizeDirection() methods.
    
    docs: Add in line comments for blocks of code

--- js/app.js
+++ js/app.js
@@ -6,28 +6,32 @@ var sprites = [
     "images/char-pink-girl.png",
     "images/char-princess-girl.png"
     ]
-// Enemies our player must avoid
+// Enemy Constructor Method.
+// Parameter: y, the y location of the horizonal path taken by the enemies.
 var Enemy = function(y) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    // Movement speed of enemy. Starts at random number between 100-300. Top end of range increases in proportion to player score.
+    this.y = y;
+    this.randomizeSpeed();
+    this.randomizeDirection();
+};
 
-    // MVP is set speed. TODO: Randomly generate enemy speed
-    this.speed = 100 + 200 * Math.random() + score/20;
+Enemy.prototype.randomizeSpeed = function() {
+    this.speed = 100 + (200 + player.score/20) * Math.random();
+};
 
-
+Enemy.prototype.randomizeDirection = function() {
+    // Randomly place enemies at the left or right edge of the screen and have them move horizonatally to the opposite side.
     if (Math.random() < 0.5) {
         this.x = -100;
     } else {
-        this.x = 505 + 100; //use canvas width
+        this.x = 505 + 100; //TO-DO: Modify code to be able to access canvas object to get width value
         this.speed = -this.speed;
     }
-
-    this.y = y; // 60,145,230
 };
+
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -36,17 +40,10 @@ Enemy.prototype.update = function(dt) {
     // which will ensure the game runs at the same speed for
     // all computers.
     this.x += dt * this.speed;
-    // console.log(" pre testing bounds");
+    // When an enemy moves off the game board, randomly assign the enemy a new speed and heading
     if (this.isOutOfBounds()) {
-        // console.log(this.x + " is out of bounds");
-
-        this.speed = 100 + 200 * Math.random() + score/20;
-        if (Math.random() < 0.5) {
-            this.x = -100;
-        } else {
-            this.x = 505 + 100; //use canvas width
-            this.speed = -this.speed;
-        }
+        this.randomizeSpeed();
+        this.randomizeDirection();
     }
 };
 
@@ -54,115 +51,102 @@ Enemy.prototype.update = function(dt) {
 Enemy.prototype.render = function() {
     if (this.speed > 0) {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-        console.log(this.sprite);
-        // ctx.fillRect(this.x ,this.y,5,5);
-    } else {
+        //console.log(this.sprite);
+    } else { // If the enemy is moving from right to left, flip the image so that the head is facing left.
         ctx.save();
         ctx.translate(this.x, this.y);
         ctx.scale(-1, 1);
-        //ctx.rotate(Math.PI);
         ctx.drawImage(Resources.get(this.sprite), 0, 0);
-        // ctx.fillRect(0,0,5,5);
-        //ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-        //ctx.rotate(Math.PI);
         ctx.restore();
     }
 
 };
 
-// outOfBounds Convenience
+// 60,145,230
+
+// outOfBounds Convenience Function - Tests to see if an enemy moves off the game board. To ensure that the sprite is able to fully
+// scroll off the screen, out of bounds is treated a 100px out of the viewable area.
 Enemy.prototype.isOutOfBounds = function() {
-    // console.log("testing bounds");
-    // console.log(canvas.width);
-    // console.log(this.x);
-    if (this.x > canvas.width + 100 || this.x < - 100) {
+    if (this.x > canvas.width + 100 || this.x < -100) {
         return true;
     } else {
         return false;
     }
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+// Player Contstructor Method
 var Player = function() {
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.lives = 3;
+    this.score = 0;
     this.sprite = 'images/char-cat-girl.png';
     this.x = 200;//0,100,200,300,400
     this.y = 370;//50,130,210,290,370
 
 }
+// Player Update function listens for keyboard input, moves the player on the gameboard, and handles the logic of player reaching top of screen
 Player.prototype.update = function(){
     Player.prototype.handleInput = function(input){
-    var verticalStep = 80;
-    var horizontalStep = 100;
+    var verticalStep = 80; //pixels to vertically move player one square
+    var horizontalStep = 100; //pixels to horizonally move player one square
     switch (input) {
         case "up":
-            if ( this.y - verticalStep > 0) {
-                // console.log ((this.y - verticalStep) + " > " + 0);
+            if ( this.y - verticalStep > 0) { //If there is a space above the player, move the player up
                 this.y -= verticalStep;
-            } else {
-                score += 100;
+            } else { //If the player reaches the top of the gameboard, respawn at the center of bottom row and add 100 points
+                this.score += 100;
+                console.log(this.score);
                 this.x = 200;//0,100,200,300,400
                 this.y = 370;//50,130,210,290,370
             }
             break;
         case "down":
-            if ( this.y + verticalStep < canvas.height -160) {
-                // console.log ((this.y + verticalStep) + " < " + (canvas.height- 160));
+            if ( this.y + verticalStep < canvas.height -160) { // If there is a space below the player, move the player down
                 this.y += verticalStep;
             }
             break;
         case "left":
-            if ( this.x - horizontalStep >= 0) {
+            if ( this.x - horizontalStep >= 0) { // If there is a space to the left of the player, move the player left
                 this.x -= horizontalStep;
-                // console.log(this.x);
             }
             break;
         case "right":
-            if ( this.x + horizontalStep <= canvas.width-105) {
+            if ( this.x + horizontalStep <= canvas.width-105) { // If there is a space to the right of the player, move the player right
                 this.x += horizontalStep;
-                // console.log(this.x  + " <= " + (canvas.width-105));
             }
             break;
     }
-    // if (input === "up") {
-    //     this.y -= 80;
-    // }
-    console.log(input);
 };
 };
 Player.prototype.render = function(){
+
+    //Draw the Player's score and # of lives remaining
     ctx.font = "30px Arial";
     ctx.clearRect(0,0,200,30);
-    ctx.fillText("Score: " + score, 10, 30);
+    ctx.fillText("Score: " + this.score, 10, 30);
     ctx.clearRect(380,0,200,30);
     ctx.fillText("Lives: " + player.lives, 380,30);
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-    // ctx.fillRect(this.x,this.y,5,5);
-    if (this.isDead()) {
-        console.log("You died");
-        player.lives--;
-        if (player.lives <= 0) {
-            gameOver = true;
-            console.log("game over");
-        }
 
+    //Draw the Player's Sprite at it's current location
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+
+    // If the player is hit by an enemy, decrement the life counter. If the player is out of lives, set gameOver to true.
+    // If the player still hase lives, respawn at the center of the bottom row of the gameboard.
+    if (this.isHitByEnemy()) {
+        this.lives--;
+        if (this.lives <= 0) {
+            gameOver = true;
+        }
         this.x = 200;//0,100,200,300,400
         this.y = 370;//50,130,210,290,370
-    } else {
-        //console.log("not dead");
     }
 };
 
 
-// outOfBounds Convenience
+// outOfBounds Convenience Function - Tests to see if an enemy moves off the game board. To ensure that the sprite is able to fully
+// scroll off the screen, out of bounds is treated a 100px out of the viewable area.
 Player.prototype.isOutOfBounds = function() {
-    // console.log("testing bounds");
-    // console.log(canvas.width);
-    // console.log(this.x);
     if (this.x > canvas.width + 100 || this.x < - 100) {
         return true;
     } else {
@@ -170,29 +154,29 @@ Player.prototype.isOutOfBounds = function() {
     }
 };
 
-Player.prototype.isDead = function() {
+// The Player's isHitByEnemy function checks for player collision with an enemy based on the grid system
+// If the player is in the same gameboard square as an enemy, the function returns true.
+Player.prototype.isHitByEnemy = function() {
     var playerSquareX = Math.round(player.x/100);
     var playerSquareY = Math.round(player.y/80);
     var collision = false;
-    //console.log("Player in tile " + playerSquareX + ", " + playerSquareY);
     allEnemies.forEach( function(enemy) {
         let enemySquareX = 0;
+        let enemySquareY = Math.round((enemy.y)/80)
         if (enemy.speed < 0) {
             enemySquareX = Math.round((enemy.x-101)/100);
         } else {
             enemySquareX = Math.round(enemy.x/100);
         }
-        var enemySquareY = Math.round(enemy.y/80);
-        // console.log(enemy.x);
-        //console.log("Enemy in tile " + enemySquareX + ", " + enemySquareY);
         if (playerSquareX === enemySquareX && playerSquareY === enemySquareY) {
-            //console.log("collision");
             collision = true;
         };
     });
     return collision;
 }
 
+// Function to allow the user to select the class of the player character
+// Currently class only determines the skin of the player character.
 function selectClass() {
     $(".charClass").append()
     for (var i = 0; i < sprites.length; i++) {
@@ -212,32 +196,13 @@ function selectClass() {
         $( "#sprite-4-clicked" ).click(function() {
             player.sprite = sprites[4];
         });
-
-
-
-
-        // console.log(sprites[1]);
-        // ctx.font ="30px Arial";
-        // ctx.fillText("Select Class", canvas.width/2,50);
-        // ctx.drawImage(Resources.get(sprites[i]), canvas.width/5*i, 100);
-        // //classOffset += 100;
     }
-    // ctx.clearRect(0,0,canvas.width,canvas.height);
-    // sprites.forEach( function(sprite){
-    //     ctx.drawImage(Resources.get(sprite), classOffset, 10);
-
-    // });
-
-
 }
 
-
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-var allEnemies = [new Enemy(60), new Enemy(145), new Enemy(230)];
 var player = new Player;
+selectClass();
+var allEnemies = [new Enemy(60), new Enemy(145), new Enemy(230)]; // Initialize one enemy per row
+init()
 
 
 // This listens for key presses and sends the keys to your
@@ -249,6 +214,5 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
-
     player.handleInput(allowedKeys[e.keyCode]);
 });
